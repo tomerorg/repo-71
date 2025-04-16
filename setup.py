e434b4c31e720c9fcd0c8a75f48c9455658d23b3
tomerbr3456 <59730830+tomerbr3456@users.noreply.github.com>
2025-04-16 14:00:03 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="repo-71",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.25.1",
+        "pandas>=1.2.0",
+        "numpy>=1.20.0",
+    ],
+    author="",
+    author_email="",
+    description="Experimental search project using NumPy and Pytest and Celery in Python",
+    keywords="repo-71, python",
+    url="",
+)
